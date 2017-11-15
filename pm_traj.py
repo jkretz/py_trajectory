e33ff00da7699165eeb859ef772c4e311068d6c8
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2017-11-15 16:37:50 +0100

    first implementation of spatio-temporal collocation; TODO: consider some refactoring to improve readability

--- pm_traj.py
+++ pm_traj.py
@@ -9,7 +9,7 @@ import datetime
 date_flight = "20170531"
 
 # set common timestamp
-date_base = "20170101"
+date_base = "20170531"
 ts_base = datetime.datetime(int(date_base[0:4]), int(date_base[4:6]), int(date_base[6::]), 0, 0, 0)
 
 ##############################################################
@@ -32,8 +32,9 @@ for nt, time in enumerate(t_track):
 # lat/lon coordinates
 lon_track = track[:, 2]
 lat_track = track[:, 3]
-flight_track = list(zip(t_track_ts, lat_track, lon_track))
+p_track = track[:, 4]
 
+flight_track = list(zip(lat_track, lon_track))
 
 ##############################################################
 # Reading ICON input
@@ -50,26 +51,23 @@ for file_name in sorted(os.listdir(ipath_icon)):
 
     icon_data = Dataset(ifile_icon)
 
+    # get all variables in files
+    var_list_all = icon_data.variables.keys()
+    # var_list = var_list_all
+    var_list = ['time', 'thb_s', 'qc']
+    #var_list = ['time', 'thb_s']
+
     # read data for the first time
     if first_read:
         # get grid information and transform into deg
         rad2deg = 180./np.pi
         lat_grid = np.array(icon_data.variables['clat'])*rad2deg
         lon_grid = np.array(icon_data.variables['clon'])*rad2deg
-
-        # get all variables in files
-        var_list_all = icon_data.variables.keys()
-        # var_list = var_list_all
-        #var_list = ['thb_s', 'u', 'time']
-        var_list = ['thb_s']
+        
         var_in = {}
         var_info = {}
 
         # get variables
-        var_list_all = icon_data.variables.keys()
-        # var_list = var_list_all
-        #var_list = ['thb_s', 'u', 'time']
-        var_list = ['time']
         var_in = {}
         var_info = {}
 
@@ -88,83 +86,97 @@ for file_name in sorted(os.listdir(ipath_icon)):
         var_in[var] = np.concatenate((var_in[var], icon_data.variables[var][:]), axis=0)
 
 # convert timesteps into the right format
-t_icon_ts = np.zeros_like(var_in["time"])
-for nt, time in enumerate(var_in["time"]):
-    t_icon_ts[nt] = (num2date(time, var_info["time", "units"]) - ts_base).total_seconds()
-var_in["time_icon_ts"] = t_icon_ts
+if "time" in var_list:
+    t_icon_ts = np.zeros_like(var_in["time"])
+    for nt, time in enumerate(var_in["time"]):
+        t_icon_ts[nt] = (num2date(time, var_info["time", "units"]) - ts_base).total_seconds()
+    var_in["time_icon_ts"] = t_icon_ts
 
+# open dataset
+f_out = Dataset('test.nc', 'w', format='NETCDF4')
+f_out.createDimension('time', len(flight_track))
 
-exit()
+# write time and lat/lon  of flight track to file
+# time
+time_out = f_out.createVariable('time', 'f4', 'time')
+time_out.standard_name = "time"
+time_out.units = "seconds since "+ts_base.strftime('%Y-%-m-%-d %H:%M:%S')
+time_out.calendar = "proleptic_gregorian"
+time_out[:] = t_track_ts
 
+# lat/lon/p
+lat_out = f_out.createVariable('lat_track', 'f4', 'time')
+lat_out.units = 'degrees_north'
+lat_out.long_name = 'lat_flight_track'
+lat_out[:] = lat_track
 
-#
-# # read flight track
-# ipath_flight_track = "/home_local/jkretzs/work_new/ac3/xcut/plane_model_trajectory/data/flight_kml/"
-#
-# ifile_track = ipath_flight_track+"/acloud_polar5_flight_10_20170531.csv"
-# track = pd.read_csv(ifile_track).values
-# lat_track = track[:, 1]
-# lon_track = track[:, 0]
-# flight_track = list(zip(lat_track, lon_track))
-#
-# # number of points during flight
-# num_track = track.shape[0]
-#
-# # open dataset
-# f_out = Dataset('test.nc', 'w', format='NETCDF4')
-# f_out.createDimension('track', len(flight_track))
-#
-# # write lat/lon to file
-# lat_out = f_out.createVariable('lat_track', 'f4', 'track')
-# lat_out.units = "degrees_north"
-# lat_out.long_name = 'lat_flight_track'
-# lon_out = f_out.createVariable('lon_track', 'f4', 'track')
-# lon_out.units = "degrees_east"
-# lon_out.long_name = 'lon_flight_track'
-#
-# lat_out[:] = lat_track
-# lon_out[:] = lon_track
-#
-# # apply kdtree decomposition
-# tree = spatial.cKDTree(list(zip(lat_grid, lon_grid)))
-#
-# var_out = {}
-# first_w3d = True
-#
-# for var in var_list:
-#
-#     # 2D-variable
-#     if len(var_in[var].shape) == 2:
-#         # create variable in output file
-#         var_out[var] = f_out.createVariable(str(var), 'f4', 'track')
-#         var_out[var].units = var_info[var, 'units']
-#         var_out[var].long_name = var_info[var, 'long_name']
-#         # select variable on flight track
-#         for np, pts in enumerate(flight_track):
-#             idx = tree.query(pts)[1]
-#             var_out[var][np] = (var_in[var])[0, idx]
-#
-#     # 3D-variable
-#     elif len(var_in[var].shape) == 3:
-#
-#         # create vertical dimension if it is the first time to write it to file
-#         num_vertical = var_in[var].shape[1]
-#         if first_w3d:
-#             f_out.createDimension('height', num_vertical)
-#             first_w3d = False
-#
-#         # create variable in output file
-#         var_out[var] = f_out.createVariable(str(var), 'f4', ('height', 'track'))
-#         var_out[var].units = var_info[var, 'units']
-#         var_out[var].long_name = var_info[var, 'long_name']
-#         # select variable on flight track
-#         for np, pts in enumerate(flight_track):
-#             idx = tree.query(pts)[1]
-#             var_out[var][:, np] = (var_in[var])[0, :, idx]
-#
-#     else:
-#         exit('strange variable dimension; exit')
-#
-# f_out.close()
+lon_out = f_out.createVariable('lon_track', 'f4', 'time')
+lon_out.units = 'degrees_east'
+lon_out.long_name = 'lon_flight_track'
+lon_out[:] = lon_track
+
+p_out = f_out.createVariable('p_track', 'f4', 'time')
+p_out.units = 'Pa'
+p_out.long_name = 'Pressure_track'
+p_out[:] = p_track*100.
+
+# apply kdtree decomposition and find closest location/time
+tree = spatial.cKDTree(list(zip(lat_grid, lon_grid)))
+idx = np.zeros_like(t_track_ts, dtype=np.int_)
+idt = np.zeros_like(t_track_ts, dtype=np.int_)
+for i_p, pts in enumerate(flight_track):
+    idx[i_p] = tree.query(pts)[1]
+    idt[i_p] = np.int(np.argmin(np.abs(var_in["time_icon_ts"]-t_track_ts[i_p])))
+
+var_out = {}
+first_w3d = True
+
+# used ICON timestep into output
+var = "time_ICON"
+var_out[var] = f_out.createVariable(str(var), 'f4', 'time')
+var_out[var].standard_name = "model_time"
+var_out[var].units = "seconds since "+ts_base.strftime('%Y-%-m-%-d %H:%M:%S')
+var_out[var].calendar = "proleptic_gregorian"
+for i_p in range(0,len(flight_track)):
+    var_out[var][i_p] = var_in["time_icon_ts"][idt[i_p]]
+
+for var in var_list:
+
+    # skip time variable
+    if var == 'time':
+        continue
+
+    # 2D-variable
+    if len(var_in[var].shape) == 2:
+        # create variable in output file
+        var_out[var] = f_out.createVariable(str(var), 'f4', 'time')
+        var_out[var].units = var_info[var, 'units']
+        var_out[var].long_name = var_info[var, 'long_name']
+
+        # select variable on flight track
+        for i_p in range(0,len(flight_track)):
+            var_out[var][i_p] = (var_in[var])[idt[i_p], idx[i_p]]
+
+    # 3D-variable
+    elif len(var_in[var].shape) == 3:
+
+        # create vertical dimension if it is the first time to write it to file
+        num_vertical = var_in[var].shape[1]
+        if first_w3d:
+            f_out.createDimension('height', num_vertical)
+            first_w3d = False
+
+        # create variable in output file
+        var_out[var] = f_out.createVariable(str(var), 'f4', ('height', 'time'))
+        var_out[var].units = var_info[var, 'units']
+        var_out[var].long_name = var_info[var, 'long_name']
+        # select variable on flight track
+        for i_p in range(0, len(flight_track)):
+            var_out[var][:, i_p] = (var_in[var])[idt[i_p], :, idx[i_p]]
+
+    else:
+        exit('strange variable dimension; exit')
+
+f_out.close()
 
 exit()
